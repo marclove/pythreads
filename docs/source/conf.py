6230ba49e0f44bd081d6728b17a53d568b606559
Marc Love <marclove@users.noreply.github.com>
2024-07-14 10:49:57 -0400

    Add sphinx docs

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "PyThreads"
+copyright = "2024, Marc Love"
+author = "Marc Love"
+release = "0.2.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+myst_heading_anchors = 2
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
---
+++ docs/source/pythreads.rst
@@ -0,0 +1,31 @@
+API Module
+--------------------
+
+.. automodule:: pythreads.api
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Configuration Module
+------------------------------
+
+.. automodule:: pythreads.configuration
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Credentials Module
+----------------------------
+
+.. automodule:: pythreads.credentials
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Threads Module
+------------------------
+
+.. automodule:: pythreads.threads
+   :members:
+   :undoc-members:
+   :show-inheritance:
